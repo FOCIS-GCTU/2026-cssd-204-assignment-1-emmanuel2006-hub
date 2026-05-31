aaff25ca8ac8744202a10a94e56b381ee374c1c8
Lil <136263922+emmanuel2006-hub@users.noreply.github.com>
2026-05-31 00:33:57 +0000

    Submission Of Assigment1

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File : initials.py
+# Description: Printing out my initials.
+# Assignment: 1
+#
+# Name: Emmanuel Little-Tetteh
+# STUDENT ID: 2425402431
+# Email: littletetteh2005@gmail.com
+# Grader: Augustus
+#
+# On my honor, Emmanuel Little-Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print small initials line with three periods on each side
+    print()
+    print("..ELT")
+    print()
 
+    # Blank line between small and large initials
+    print("...EEEEEEEEEEEE........LLLL        ........TTTTTTTTTTTT.....")
+    print("...EEEEEEEEEEEE........LLLL        ........TTTTTTTTTTTT.....")
+    print("...EEE         ........LLLL        ........    TTT     .....")
+    print("...EEE         ........LLLL        ........    TTT     .....")
+    print("...EEEEEEEEEEEE........LLLL        ........    TTT     .....")
+    print("...EEEEEEEEEEEE........LLLL        ........    TTT     .....")
+    print("...EEE         ........LLLL        ........    TTT     .....")
+    print("...EEE         ........LLLL        ........    TTT     .....")
+    print("...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**...    TTT     ...**")
+    print("...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**...    TTT     ...**")
 
+    # Final blank line after large initials
+    print()
 
 main()
